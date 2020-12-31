00e70fe1d94238566c27f6d25c07a880c900d863
bridgetle <24bridgetle@alice-smith.edu.my>
2020-12-31 08:40:37 +0000

    Tidied it up

--- main.py
+++ main.py
@@ -23,70 +23,63 @@ def Huffman(data):
 '''
 
 
-print("Idea: the more frequent a character is, the shorter its Huffman encoding, because it's going to appear more times")
 
 # the previous comment is called a docstring
 
 # Replace each character (for example 'M', 't', 'b', etc) by its Huffman encoding (ex: 'M' corresponds to "10")return "0110110"
-
+print("Idea: the more frequent a character is, the shorter its Huffman encoding, because it's going to appear more times")
 
 input_string = "Hola ba ma"
    
 if "H" in input_string :
     string = input_string.replace("H", "101") 
-    
 else :
     print (" ")
 
 if "o" in input_string :
     string1 = string.replace("o", "100") 
-    
 else :
     print(" ")
 
 if "l" in input_string :
     string2 = string1.replace("l", "110") 
-    
 else :
     print(" ")
 
 if "m" in input_string :
     string3 = string2.replace("m", "001") 
-    
 else :
     print(" ")
 
 if "b" in input_string :
     string4 =  string3.replace("b", "01") 
-    
 else :
     print(" ")
     
 if "a" in input_string :
     string5 = string4.replace("a", "10")
-    
 else :
     print(" ")
 
 if " " in input_string :
     string6 = string5.replace(" ", "11")
-    print(string6)
 else :
     print(" ")
+
+print(string6)
 
 
 
 
 '''
 the "key" :
-
-string = input_string.replace("H", "101")
-string = input_string.replace("o", "100")
-string = input_string.replace("l", "110")
-string = input_string.replace("m", "001")
-string = input_string.replace("b", "01")
-string = input_string.replace("a", "10")
-string = input_string.replace(" ", "11")
+H = 101
+o = 100
+l = 110
+m = 001
+b = 01
+a = 10
+  = 11
 
 '''
 
